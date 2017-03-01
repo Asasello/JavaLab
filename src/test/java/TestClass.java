c449278c3c923f9c5b301d0ae9bf14ff8da72b46
Jaroslaw Rolinski <asasello225@gmail.com>
2017-03-01 20:31:37 +0100

    Added test test.

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Asasello on 01-Mar-17.
+ */
+
+public class TestClass {
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+    }
+}
